f56b7ebffa9b01a4802d5720fbdc6413f6bc9bbc
Benjamin Koch <bbbsnowball@gmail.com>
2016-10-15 02:19:16 +0200

    refactor Artnet sink

--- apps/sink-dmx-artnet/dmx-lamp.rb
+++ apps/sink-dmx-artnet/dmx-lamp.rb
@@ -15,8 +15,6 @@ def config
   $program_path = './programs/'
   $channel_write_interval = 0.01
   $debug = false
-  $lamps = [2, 6, 10, 14, 18, 22, 25, 30, 34]
-  $default_colors = ["backgroundA","backgroundB","backgroundB","backgroundB","backgroundB", "green", "green", "backgroundA", "backgroundA"]
   $server = {
     0  => ["172.31.65.70", "172.31.64.110"],
     30 => ["172.31.65.74", "172.31.65.196"],
@@ -27,6 +25,19 @@ def config
   $server[12] = $server[10]
   $quiet_time = 10
 end  #/config
+
+def config_lamps(c)
+  # id, universe, address, default program
+  c.add_lamp( 2, 0,  2, "backgroundA")
+  c.add_lamp( 6, 0,  6, "backgroundB")
+  c.add_lamp(10, 0, 10, "backgroundB")
+  c.add_lamp(14, 0, 14, "backgroundB")
+  c.add_lamp(18, 0, 18, "backgroundB")
+  c.add_lamp(22, 0, 22, "green")
+  c.add_lamp(25, 0, 25, "green")
+  c.add_lamp(30, 0, 30, "backgroundA")
+  c.add_lamp(34, 0, 34, "backgroundA")
+end
 
 require "bunny"
 require "socket"
@@ -48,21 +59,18 @@ class Artnet
     end
   end
 
-  def possibly_send(universe, maxchannel, channels)
+  def possibly_send(universe)
     @sema.synchronize do
-      return if @time_of_recent_message_for_universe[universe] + $quiet_time > Time.now
+      return if @time_of_recent_message_for_universe[universe.id] + $quiet_time > Time.now
+      return unless $server[universe.id]
 
-      #TODO make sure the string is not unicode
-      cnt = maxchannel + 1
-      msg = "Art-Net\0\0\x50\0\0\0\0\0\0\0\x00" + "\0"*cnt
-      msg[17] = (cnt&0xff).chr
-      msg[16] = ((cnt>>8)&0xff).chr
-      msg[14] = (universe&0xff).chr
-      msg[15] = ((universe>>8)&0xff).chr
-      channels.each_pair do |channel, value|
-        msg[18+channel] = (value.to_i&0xff).chr
-      end
-      $server[universe].each do |server|
+      channels = universe.channel_data
+      msg = "Art-Net\0\0\x50\0\0\0\0\0\0\0\x00" + channels
+      msg[17] = (channels.length&0xff).chr
+      msg[16] = ((channels.length>>8)&0xff).chr
+      msg[14] = (universe.id&0xff).chr
+      msg[15] = ((universe.id>>8)&0xff).chr
+      $server[universe.id].each do |server|
         @socket.send(msg, 0, server, 6454)
       end
     end
@@ -98,99 +106,189 @@ class Artnet
   end
 end
 
-class DmxControl
-  def initialize
-    @sema = Mutex.new
-    @channels = {}
-    @programs = {}
-    @enabled = true
-    @maxchannel = 0
-    @artnet = Artnet.new
- end
-
-  def on
-    @sema.synchronize {
-      $lamps.each_pair { |lampid, default|
-        setprogram(lampid, default)
-      }
-      @enabled = true
-    }
+class DmxLamp
+  def initialize(universe, address, default_program)
+    @universe = universe
+    @address = address
+    @default_program = ColorProgram.resolve(default_program)
+    @program = @default_program
   end
 
-  def off
-    @sema.synchronize {
-      @enabled = false
-    }
+  attr_reader :universe, :address, :program
+
+  def program=(value)
+    @program = ColorProgram.resolve(value)
   end
 
-  def setchannel(channel, value)
-    @channels[channel] = value
-    if channel > @maxchannel then
-      @maxchannel = channel
+  def reset_program
+    @program = @default_program
+  end
+
+  def advance_program
+    @program.next
+  end
+
+  def maxchannel
+    @address + 3
+  end
+
+  def update_channels(channels)
+    channels[@address-1] = 0
+    @program.current.each_with_index do |value, index|
+      channels[@address+index] = [value, 0, 255].sort[1]
+    end
+    channels[@address+3] = 0
+  end
+end
+
+class Channels
+  def initialize(count)
+    @data = "\0"*count
+  end
+
+  attr_reader :data
+
+  def length
+    @data.length
+  end
+
+  def [](index)
+    @data[index].ord
+  end
+
+  def []=(index, value)
+    @data[index] = value.to_i.chr
+  end
+end
+
+class DmxUniverse
+  def initialize(universe)
+    @id = universe
+    @objects = []
+    @maxchannel = -1
+    @channels = Channels.new(0)
+  end
+
+  attr_reader :id
+
+  def each
+    @objects.each do |obj|
+      yield obj
     end
   end
 
-  def setprogram(channel, color)
-    return unless $lamps.include? channel
-    @programs[channel] = color
-    setchannels
+  def <<(obj)
+    raise "Wrong universe" if obj.universe != @id
+    if obj.maxchannel > @maxchannel
+      @maxchannel = obj.maxchannel
+    end
+    @objects << obj
+  end
+
+  def channel_data
+    @channels = Channels.new(@maxchannel+1) if @maxchannel != @channels.length
+    @objects.each do |obj|
+      obj.update_channels(@channels)
+    end
+    return @channels.data
+  end
+end
+
+class DmxUniverses
+  def initialize
+    @universes = {}
+  end
+
+  def [](index)
+    if @universes.include? index
+      @universes[index]
+    else
+      u = DmxUniverse.new(index)
+      @universes[index] = u
+      return u
+    end
+  end
+
+  def each
+    @universes.each_value do |obj|
+      yield obj
+    end
+  end
+
+  def <<(obj)
+    self[obj.universe] << obj
+  end
+end
+
+class DmxControl
+  def initialize
+    @sema = Mutex.new
+    @enabled = true
+    @universes = DmxUniverses.new
+    @artnet = Artnet.new
+    @lamps = {}
+  end
+
+  def add_lamp(id, universe, address, default_program)
+    @sema.synchronize do
+      lamp = DmxLamp.new(universe, address, default_program)
+      @lamps[id] = lamp
+      @universes << lamp
+    end
+  end
+
+  def on
+    @sema.synchronize do
+      @lamps.each_pair do |lampid, lamp|
+        lamp.reset_program
+      end
+      @enabled = true
+    end
+  end
+
+  def off
+    @sema.synchronize do
+      @enabled = false
+    end
+  end
+
+  def setprogram(lampid, program_or_color)
+    raise Exception("no such lamp: #{lampid}") unless @lamps.include? lampid
+    @sema.synchronize do
+      @lamps[lampid].program = program_or_color
+    end
   end
 
   def loop
     while true
       sleep($channel_write_interval)
       next unless @enabled
-      advanceprograms
-      @sema.synchronize {
-        @artnet.possibly_send(0, @maxchannel, @channels)
-      }
+      @sema.synchronize do
+        @lamps.each_pair do |lampid, lamp|
+          lamp.advance_program
+        end
+        @universes.each do |universe|
+          @artnet.possibly_send(universe)
+        end
+      end
     end
   end
-
-  private
-
-  def advanceprograms
-    @programs.each_pair do |channel, color|
-      color.next
-    end
-    setchannels
-  end
-
-  def setchannels
-    @programs.each_pair do |channel, color|
-      setrgb(channel, *color.current)
-    end
-  end
-
-  def setrgb(channel, r, g, b)
-    r = 255 if r > 255
-    r = 0 if r < 0
-    g = 255 if g > 255
-    g = 0 if g < 0
-    b = 255 if b > 255
-    b = 0 if b < 0
-    setchannel(channel-1, 0)
-    setchannel(channel+0, r)
-    setchannel(channel+1, g)
-    setchannel(channel+2, b)
-    setchannel(channel+3, 0)
-  end
-
 end
 
-class Color
+class ColorProgram
   @@colors = {}
 
   def self.resolve(color)
+    return color if color.is_a?(ColorProgram)
     col = simple_resolve(color)
-    return Color.new([col]) if col
+    return ColorProgram.new([col]) if col
     p = File.join($program_path,color+".rb")
     if File.exist?(p) && File.realpath(p).start_with?($program_path) then
       c = load_color_subclass(p)
       return c if c
     end
     p = File.join($program_path,color)
-    return Color.new(p) if File.exist?(p) && File.realpath(p).start_with?($program_path)
+    return ColorProgram.new(p) if File.exist?(p) && File.realpath(p).start_with?($program_path)
     fail "Unbekannte Farbe: #{color}"
   end
   def self.simple_resolve(color)
@@ -224,7 +322,7 @@ class Color
       @colors = []
       @index = 0
       File.open(colors).each do |line|
-        c = Color.simple_resolve(line.strip)
+        c = ColorProgram.simple_resolve(line.strip)
         @colors << c if c
       end
       puts "loaded Program #{colors} with #{@colors.length} lines"
@@ -241,47 +339,66 @@ class Color
     @index = (@index + 1) % @colors.length
   end
 end
+Color = ColorProgram
 
-config
-$program_path = File.realpath($program_path)
-Color.load_colors
-lamps = $lamps
-$lamps = {}
-lamps.each_index { |i| $lamps[lamps[i]] = Color.resolve($default_colors[i])}
-
-if __FILE__ == $0
-  rkprefix = "dmx.lamp."+$subsystem+"."
-  control = DmxControl.new
-  control.on
-
-  conn = Bunny.new(:host => ENV.fetch("AMQP_SERVER", "mopp"))
-  conn.start
-  ch = conn.create_channel
-  xchg = ch.topic("act_dmx", :auto_delete => true)
-  q = ch.queue("act_dmx_subraum", :auto_delete => true)
-  q.bind(xchg, :routing_key => rkprefix+"*").subscribe do |info, meta, data|
-    rk = info.routing_key
-    puts "#{rk}: #{data}"
-    if rk == rkprefix+"control"
-      case data
-      when "on" then
-        control.on
-      when "off" then
-        control.off
-      end
-      next
-    end
-    lamp = rk[rkprefix.length..-1]
-    next unless lamp
-    begin
-      program = Color.resolve(data)
-      puts "Setting #{lamp} to #{program}"
-      control.setprogram(lamp.to_i, program)
-    rescue => err
-      puts err
+class EdiClient
+  def initialize(dmx_control, host, routing_key_prefix)
+    @dmx_control = dmx_control
+    @routing_key_prefix = routing_key_prefix
+    @conn = Bunny.new(:host => host)
+    @conn.start
+    ch = @conn.create_channel
+    xchg = ch.topic("act_dmx", :auto_delete => true)
+    q = ch.queue("act_dmx_subraum", :auto_delete => true)
+    q.bind(xchg, :routing_key => routing_key_prefix+".*").subscribe do |info, meta, data|
+      handle_message(info, meta, data)
     end
   end
 
-  control.loop
-  conn.close
+  def handle_message(info, meta, data)
+    rk = info.routing_key
+    puts "#{rk}: #{data}"
+    return unless rk.start_with?(@routing_key_prefix + ".")
+    rk = rk[@routing_key_prefix.length+1..-1]
+    case rk
+    when "control"
+      case data
+      when "on" then
+        @dmx_control.on
+      when "off" then
+        @dmx_control.off
+      end
+    else
+      lamp = rk
+      begin
+        program = ColorProgram.resolve(data)
+        puts "Setting #{lamp} to #{program}"
+        @dmx_control.setprogram(lamp.to_i, program)
+      rescue => err
+        puts "Error while handling message with rk=#{rk.inspect}, data=#{data.inspect}: #{err}"
+      end
+    end
+  end
+
+  def close
+    @conn.close
+  end
+end
+
+config
+$program_path = File.realpath($program_path)
+ColorProgram.load_colors
+
+if __FILE__ == $0
+  dmx_control = DmxControl.new
+  config_lamps(dmx_control)
+  dmx_control.on
+
+  edi_client = EdiClient.new(
+    dmx_control,
+    ENV.fetch("AMQP_SERVER", "mopp"),
+    "dmx.lamp.#{$subsystem}")
+
+  dmx_control.loop
+  edi_client.close
 end
